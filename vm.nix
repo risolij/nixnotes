fa58507ac5e838b1d7c74aaee8387b51da3cd6b1
risolij <risolij@gmail.com>
2022-01-19 20:01:45 -0700

    initial documentation repo

---
+++ vm.nix
@@ -0,0 +1,48 @@
+# vm.nix => A custom VM to use
+{ 
+  test1 = { pkgs, lib, config, ... }:
+  with lib;
+  {
+    imports = [
+      <nixpkgs/nixos/modules/profiles/qemu-guest.nix>
+      <nixpkgs/nixos/modules/virtualisation/qemu-vm.nix>
+    ];
+    networking = {
+      hostName = "temppc";
+      wireless.enable = true;
+      networkmanager.enable = true;
+    };
+
+    users.users.tempguy = {
+      extraGroups = [ "networkmanager" "wheel" ];
+      initialPassword = "test";
+      isNormalUser = true;
+    };
+
+    virtualisation = {
+      cores = 2;
+      diskSize = 20 * 1024; # MB
+      memorySize = 6 * 1024; # MB
+      writableStoreUseTmpfs = false;
+    };
+
+    environment.systemPackages = with pkgs; [
+      vim
+      firefox
+    ];
+
+    services = {
+      xserver = {
+        enable = true;
+        layout = "us";
+        desktopManager.mate = {
+          enable = true;
+        };
+      };
+      sshd = {
+        enable = true;
+      };
+      qemuGuest.enable = true;
+    };
+  };
+}
